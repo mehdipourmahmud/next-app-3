aa225fec437c50f15541f621462564dc8f37c9ca
Mehdi pourmahmud <108904661+mehdipourmahmud@users.noreply.github.com>
2023-07-27 10:42:53 +1000

    Update actions.tsx

--- libs/actions.tsx
+++ libs/actions.tsx
@@ -29,7 +29,7 @@ export const createNewProject = async (projectData,name,email) => {
       description: projectData.description,
       image: projectData.image,
       liveSiteURL: projectData.liveSiteURL,
-      githudURL: projectData.githudURL, // Change 'githudURL' to 'githubURL'
+      githubURL: projectData.githudURL, // Change 'githudURL' to 'githubURL'
       category: projectData.category,
       createdBy: {
         name: name,
